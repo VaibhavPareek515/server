564fb5f2b6122c9d389b13d1dd11c6a2c43a86d4
Vaibhav Pareek <vaibhavpareek515@gmail.com>
2024-08-03 14:10:15 +0530

    added cors

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const {createPool} = require('mysql');
 
 const app = express();
 app.use(cors());
+app.options('*',cors());
+var allowCrossDomain = function(req,res,next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();  
+}
+app.use(allowCrossDomain);
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
